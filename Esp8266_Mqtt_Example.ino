d9486d2b5fd54e5dde76774bfe57855bd62b65be
jedsada <jt@20scoops.com>
2017-03-23 22:52:54 +0700

    control by mqtt.

--- Esp8266_Mqtt_Example.ino
+++ Esp8266_Mqtt_Example.ino
@@ -1,22 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
-
 const char* ssid = "20 Laboratory";
 const char* password = "mg;o9uhld^Ux";
 const char* mqtt_server = "broker.mqtt-dashboard.com";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
 char msg[50];
-int value = 0;
 
 void setup_wifi() {
-
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -43,66 +37,58 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
+
   Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
+  controlPins((char)payload[0] == '0', (char)payload[0]);
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
+      client.subscribe("20scoopsClawMachine");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
 }
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(15, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  client.subscribe("20scoopsClawMachine");
 }
 
 void loop() {
-
-  if (!client.connected()) {
-    reconnect();
-  }
+  if (!client.connected())reconnect();
   client.loop();
+}
 
-  long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, 75, "https://medium.com/@pondthaitay  #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("20scoopsClawMachine", msg);
-//    client.subscribe("outTopic");
+void controlPins(bool flag, char pin) {
+  if (!flag) {
+    if (pin == '1') digitalWrite(0, HIGH);
+    else if (pin == '2') digitalWrite(2, HIGH);
+    else if (pin == '3') digitalWrite(15, HIGH);
+    else if (pin == '4') digitalWrite(4, HIGH);
+    else if (pin == '5') digitalWrite(5, HIGH);
+  } else {
+    digitalWrite(0, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(15, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
   }
 }
